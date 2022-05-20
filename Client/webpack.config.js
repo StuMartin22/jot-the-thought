428deb1dd77f36e0adf6d8e6629367559d652703
StuMartin22 <STUCODES22@GMAIL.COM>
2022-05-19 23:30:18 -0500

    got npm run build within client level working. Added event listeners within the install.js on client level.

--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -18,74 +18,48 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-        
-      ],
+      new HtmlWebpackPlugin ({
+        template: './index.html',
+        title: 'jot-the-thought',
+        favicon: './favicon.ico'
+      }),
+      new InjectManifest({
+        // These are some common options, and not all are required.
+        // Consult the docs for more info.
+        swSrc: './src-sw.js',
+        swDest: './src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        name: 'Jot the Thought',
+        short_name: 'jtt',
+        description: 'My awesome Progressive Web App!',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          },
+        ]
+      }),
+    ],
       module: {
         rules: [
           {
             test: /\.css$/i,
             use: ["style-loader", "css-loader"],
           },
-        ],
-        module: {
-          rules: [
             {
               test: /\.m?js$/,
               exclude: /(node_modules|bower_components)/,
               use: {
                 loader: 'babel-loader',
                 options: {
-                  presets: ['@babel/preset-env']
-                }
+                  presets: ['@babel/preset-env'],
+                  plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+                },
               }
             }
-          ],
-          module: {
-            rules: [
-              { test: /\.css$/, use: 'css-loader' },
-              { test: /\.ts$/, use: 'ts-loader' },
-            ],
-          },
-          plugins: [
-            // Other plugins...
-            new InjectManifest({
-              // These are some common options, and not all are required.
-              // Consult the docs for more info.
-              exclude: [/.../, '...'],
-              swSrc: '...',
-            }),
-          ],
-          plugins: [
-            new WebpackPwaManifest({
-              name: 'My Progressive Web App',
-              short_name: 'MyPWA',
-              description: 'My awesome Progressive Web App!',
-              background_color: '#ffffff',
-              crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
-              icons: [
-                {
-                  src: path.resolve('src/assets/icon.png'),
-                  sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
-                },
-                {
-                  src: path.resolve('src/assets/large-icon.png'),
-                  size: '1024x1024' // you can also use the specifications pattern
-                },
-                {
-                  src: path.resolve('src/assets/maskable-icon.png'),
-                  size: '1024x1024',
-                  purpose: 'maskable'
-                }
-              ]
-            })
-          ],
-          entry: 'index.js',
-          output: {
-            path: path.resolve(__dirname, './dist'),
-            filename: 'index_bundle.js',
-          },
-          plugins: [new HtmlWebpackPlugin()],
+          ]
         }
-    },
+    }
   };
-};
